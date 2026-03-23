ef383da6584771887d84bded88c8a9237513e338
francois.le.goff <flegoff.dev@gmail.com>
2026-03-23 16:53:06 +0100

    -Renommage des packages `com.example.demo` -> `com.accenture.pizzeria` -Renommage de `DemoApplication` en `PizzeriaApplication`

--- src/main/java/com/accenture/pizzeria/PizzeriaApplication.java
+++ src/main/java/com/accenture/pizzeria/PizzeriaApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.accenture.pizzeria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PizzeriaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(PizzeriaApplication.class, args);
     }
 
 }
--- src/test/java/com/accenture/pizzeria/PizzeriaApplicationTests.java
+++ src/test/java/com/accenture/pizzeria/PizzeriaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.accenture.pizzeria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PizzeriaApplicationTests {
 
     @Test
     void contextLoads() {
