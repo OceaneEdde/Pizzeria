e602a10fd5011fdb5b95463634c6adb4c0f26725
Lupercall29 <ami.pierrot@outlook.fr>
2026-03-24 11:18:08 +0100

    Revert "Customer dtos"

--- src/main/java/com/accenture/pizzeria/model/Address.java
+++ src/main/java/com/accenture/pizzeria/model/Address.java
@@ -1,19 +1,4 @@
 package com.accenture.pizzeria.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
-
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Address {
-    private UUID id;
-    private String street;
-    private String city;
-    private String postalCode;
 }
--- src/main/java/com/accenture/pizzeria/model/Customer.java
+++ src/main/java/com/accenture/pizzeria/model/Customer.java
@@ -1,28 +1,4 @@
 package com.accenture.pizzeria.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.UUID;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
 public class Customer {
-    private UUID uuid;
-    private String firstName;
-    private String lastName;
-    private String email;
-    List<Order> orders;
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id")
-    private Address address;
-    private Boolean isVIP;
 }
