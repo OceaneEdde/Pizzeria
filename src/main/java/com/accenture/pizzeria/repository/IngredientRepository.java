842fff3fbe454c7757099143dc63efa8315bc63b
francois.le.goff <flegoff.dev@gmail.com>
2026-03-25 11:35:46 +0100

    -Tests unitaire sur findAll de  `IngredientServiceImpl` -Tests unitaires sur findById de  `IngredientServiceImpl` -Tests unitaires sur findByName de  `IngredientServiceImpl`
    -Update de `IngredientRepository`, passage en `JpaRepository`

--- src/main/java/com/accenture/pizzeria/repository/IngredientRepository.java
+++ src/main/java/com/accenture/pizzeria/repository/IngredientRepository.java
@@ -1,9 +1,12 @@
 package com.accenture.pizzeria.repository;
 
 import com.accenture.pizzeria.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, UUID> {
+public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
+    Optional<Ingredient> findByName(String name);
 }
--- src/main/java/com/accenture/pizzeria/service/IngredientService.java
+++ src/main/java/com/accenture/pizzeria/service/IngredientService.java
@@ -4,6 +4,15 @@ import com.accenture.pizzeria.exception.PizzeriaException;
 import com.accenture.pizzeria.service.dto.IngredientRequestDto;
 import com.accenture.pizzeria.service.dto.IngredientResponseDto;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface IngredientService {
     IngredientResponseDto addIngredient(IngredientRequestDto requestDto) throws PizzeriaException;
+
+    List<IngredientResponseDto> findAll();
+
+    IngredientResponseDto findById(UUID id) throws PizzeriaException;
+
+    IngredientResponseDto findByName(String name) throws PizzeriaException;
 }
