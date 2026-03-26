169a900857f361d7c8ea470ae8a68aa9f534a008
francois.le.goff <flegoff.dev@gmail.com>
2026-03-26 11:53:51 +0100

    -Tests unitaires sur `CustomerServiceImpl`, méthode findByEmail. -Tests d'intégration sur `CustomerController` -Tests E2E sur `CustomerController`
    -Ajout de la méthode `findByEmail` dans `CustomerRepository`
    -Ajout de la méthode `findByEmail` dans `CustomerService`
    -Implémentation de la méthode `findByEmail` dans `CustomerServiceImpl`
    -Création de la route `/customers/email/{email}` dans `CustomerApi`
    -Implémentation de la route `/customers/email/{email}` dans `CustomerController`
    -Logs & JavaDoc

--- src/main/java/com/accenture/pizzeria/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/accenture/pizzeria/service/impl/CustomerServiceImpl.java
@@ -7,12 +7,15 @@ import com.accenture.pizzeria.repository.CustomerRepository;
 import com.accenture.pizzeria.service.CustomerService;
 import com.accenture.pizzeria.service.dto.CustomerRequestDto;
 import com.accenture.pizzeria.service.dto.CustomerResponseDto;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -25,6 +28,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     /**
      * Service method to add a new customer in the database
+     *
      * @param requestDto a CustomerRequestDto
      * @return a CustomerResponseDto, reflecting the newly created Customer
      * @throws PizzeriaException when the DTO is malformed or null
@@ -38,16 +42,35 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     /**
+     * Service method to find a customer by it's email in the database
+     *
+     * @param email a String containing the email we are querying the database with to find our Customer
+     * @return a CustomerResponseDto, reflecting the found Customer
+     * @throws PizzeriaException when the email is null or blank
+     */
+    @Override
+    public CustomerResponseDto findByEmail(String email) throws PizzeriaException {
+        log.info("Accessing CUSTOMER Service Method : findByEmail");
+        if (null == email || email.isBlank())
+            throw new PizzeriaException(messages.getMessage("customer.mail.invalid"), HttpStatus.BAD_REQUEST);
+        Optional<Customer> optCustomer = customerRepository.findByEmail(email);
+        if (optCustomer.isEmpty())
+            throw new EntityNotFoundException(messages.getMessage("customer.notfound"));
+        Customer customer = optCustomer.get();
+        return customerMapper.toResponseDto(customer);
+    }
+
+    /**
      * Service method to verify the CustomerRequestDtos
+     *
      * @param requestDto any CustomerRequestDto
      * @throws PizzeriaException when the DTO is malformed or null
      */
     @Override
     public void verify(CustomerRequestDto requestDto) throws PizzeriaException {
         log.info("Accessing CUSTOMER Service Method : verify");
-        if(null==requestDto)
+        if (null == requestDto)
             throw new PizzeriaException(messages.getMessage("dto.null"), HttpStatus.BAD_REQUEST);
     }
-
 
 }
